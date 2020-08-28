b33e638555cd6f2ad74ab40f523603a2c07f00f2
fgalettilivingwd <franciscogaletti@yahoo.com>
2020-08-28 11:48:49 -0500

    75 Using a delegate

---
+++ EventsCallback/EventsCallback/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsCallback
+{
+    class Program
+    {
+        // 75 Using a delegate
+        static void Main(string[] args)
+        {
+            UseDelegate del = new UseDelegate();
+
+        }
+    }
+}
---
+++ EventsCallback/EventsCallback/UseDelegate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsCallback
+{
+    public class UseDelegate
+    {
+        public delegate int Calculate(int x, int y);
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public UseDelegate()
+        {
+            Calculate calc = Add; // pointing to Add
+
+            Console.WriteLine(calc(3, 4));
+
+            calc = Multiply; // 
+
+            Console.WriteLine(calc(3, 4));
+        }
+
+    }
+}
